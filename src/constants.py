db412cb2f17bf1a02491f3b69786b14ccbcd5596
Dr Anirban Mitra <67600799+mitradranirban@users.noreply.github.com>
2020-10-19 08:16:00 +0530

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,8 @@
-"""
-Global constants
-"""
+"""Global constants"""
 
 # Colors
-BLACK    = (   0,   0,   0) 
-WHITE    = ( 255, 255, 255) 
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
 BLUE     = (   0,   0, 255)
 
 # Screen dimensions
