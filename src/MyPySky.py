a2520b1be10e38fcb8cb7d25ffd617826e7c0911
Dr Anirban Mitra <67600799+mitradranirban@users.noreply.github.com>
2020-10-23 22:50:28 +0530

    Update MyPySky.py
    Added enemy code

--- src/MyPySky.py
+++ src/MyPySky.py
@@ -12,6 +12,7 @@ http://programarcadegames.com/python_examples/sprite_sheets/
 import pygame
 from pygame.locals import *
 import os
+import random
 
 
 # Colors
@@ -175,8 +176,48 @@ class Player(pygame.sprite.Sprite):
         """ Called when the user lets off the keyboard. """
         self.change_x = 0
     
+class Enemy(pygame.sprite.Sprite):
 
-""" Module for managing platforms. """
+    ''' Spawn an enemy '''
+               
+    def __init__(self,x,y,img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images',img))
+        self.movey = 0
+        self.image.convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.counter = 0
+
+    def move(self):
+        ''' enemy movement '''
+        distance = random.randint(10,40)
+        speed = random.randint(1,4)
+        self.movey += 1.2
+    
+        if self.counter >= 0 and self.counter <= distance:
+            self.rect.x += speed
+        elif self.counter >= distance and self.counter <= distance*2:
+            self.rect.x -= speed
+        else:
+            self.counter = 0
+       
+        self.counter += 1
+
+        if not self.rect.y >= SCREEN_HEIGHT - TILEX:
+            self.rect.y += self.movey
+
+        plat_hit_list = pygame.sprite.spritecollide(self, levels.platform_list, False)
+        for p in plat_hit_list:
+            self.movey = 0
+            if self.rect.y > p.rect.y:
+                self.rect.y = p.rect.y+constants.TILEY
+            else:
+                self.rect.y = p.rect.y-constants.TILEY
+               
+               
+""" Create the  platforms. """
 
 
 def load_image(self, width, height):
